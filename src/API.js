4af4573f57949db81898ba6769706650d2306cad
Rafael Falk <rafaelmartinsfalk2@gmail.com>
2023-02-25 03:11:17 -0300

    API configuration

---
+++ src/API.js
@@ -0,0 +1,61 @@
+const API_KEY = "bbd9548ca40094e1de167abba96ec747";
+const API_BASE = "https://api.themoviedb.org/3";
+
+/*
+-netflix original 
+-trendings
+-top rated 
+-movie list(action,comedy,terror,romance...)
+*/
+
+const basicFetch = async (endpoint) => {
+  const req = await fetch(`${API_BASE}${endpoint}`);
+  const json = await req.json();
+  return json;
+}
+export default {
+  getHomeList: async () => {
+    return [
+      {
+        slug:'originals',
+        title:'Netflix Originals',
+        items: await basicFetch(`/discover/tv?with_networks=213&language=pt-BR&api_key=${API_KEY}`) 
+      },
+      {
+        slug:'trendings',
+        title:'Trendings',
+        items: await basicFetch(`/trending/all/week?language=ptBR&api_key=${API_KEY}`)
+      },
+      {
+        slug:'toprated',
+        title:'Top Rated',
+        items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug:'action',
+        title:'Action',
+        items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug:'comedy',
+        title:'Comedy',
+        items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug:'horror',
+        title:'Horror',
+        items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug:'romance',
+        title:'Romance',
+        items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug:'documentary',
+        title:'Documentary',
+        items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+      },
+    ];
+  }
+}
