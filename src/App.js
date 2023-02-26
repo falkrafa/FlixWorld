8d30b3bfb155bb46b16bdcde00503d10e050ff1a
falkrafa <rmf2@cesar.school>
2023-02-25 22:33:03 -0300

    starting structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React, {useEffect, useState} from "react";
 import API from './API';
 import MovieRow from "./components/MovieRow";
+import "./App.css";
+import Featuredmovie from './components/FeaturedMovie';
 
 export default () => {
 
   const [movielist, setMovieList] = useState([]);
+  const [featuredData, setFeatureData] = useState(null)
 
   useEffect(()=>{
     const loadAll = async () =>{
       let list = await API.getHomeList();
       setMovieList(list);
+
+      // getting featured movie 
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomOriginals = Math.floor(Math.random()*(originals[0].items.results.length-1));
+      let chosen =  originals[0].items.results[randomOriginals];
+      let chosenInfo = await API.getMovieInfo(chosen.id, 'tv');
+      setFeatureData(chosenInfo);
     }
     loadAll();
   },[]);
   return(
     <div className="page">
+
+      {featuredData && 
+        <Featuredmovie item={featuredData}/>
+      }
       <section className="lists">
         {movielist.map((item,key)=>(
           <MovieRow key={key} title ={item.title} items={item.items}/>
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./MovieRow.css";
 export default ({title,items}) => {
   return (
-    <div>
+    <div className="movieRow">
       <h2>{title}</h2>
       <div className="movieRow-area">
-        {items.results.length > 0 && items.results.map((item, key)=>(
-          <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.originals_title}/>
-        ))}
+        <div className="movieRow-list">
+          {items.results.length > 0 && items.results.map((item, key)=>(
+            <div key={key}className="movieRow-item">
+              <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.originals_title}/>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
